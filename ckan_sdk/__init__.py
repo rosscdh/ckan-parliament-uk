6258b30302a8c4c1508886025cfe85bb4893e1cc
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-31 12:18:33 +0200

    optimisations

--- ckan_sdk/__init__.py
+++ ckan_sdk/__init__.py
@@ -5,14 +5,27 @@ import requests
 class Parla(object):
     r = requests
     base_url = 'http://demo.ckan.org/api/3/action/'
-    resp = None
+    resp = None  # response from server
+    resp_status = None # shortcut to response.status_code
+    resp_headers = None # shortcut to response.status_code
+
+    def __init__(self, *args, **kwargs):
+        self.resp = None
+        self.resp_status = None
+        self.resp_headers = None
 
     @property
     def endpoint(self):
         return '%s%s' % (self.base_url, self.action)
 
+    def response(self, resp):
+        self.resp = resp
+        self.resp_status = resp.status_code
+        self.resp_headers = resp.headers
+        return resp
+
     def get(self, **kwargs):
-        self.resp = self.r.get(self.endpoint, params=kwargs)
+        self.response(self.r.get(self.endpoint, params=kwargs))
         return self.resp.json()
 
 
